243192330aaaed51428c58ca8aecc3727566b2f9
Igor Baiborodine <igor.baiborodine@yandex.com>
2019-05-10 14:44:34 -0400

    Update docker-entrypoint.sh

--- 7/alpine/docker-entrypoint.sh
+++ 7/alpine/docker-entrypoint.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e
 
-echo "cmd: $0, args: $@"
-echo "user ID:$(id -u), LIFERAY_HOME:$LIFERAY_HOME, LIFERAY_BASE:$LIFERAY_BASE, tomcat_version:$(cat tomcat_version)"
+printenv
 
-tree --noreport "$LIFERAY_BASE"
+echo "
+cmd: $0
+args: $@
+Docker user ID:$(id -u)
+tomcat_version:$(cat tomcat_version)"
 
 if [[ "$(ls -A $LIFERAY_BASE)" ]]; then
+    tree --noreport "$LIFERAY_BASE"
+
     if [[ -d "$LIFERAY_BASE/tomcat" ]]; then
         mv "$LIFERAY_BASE/tomcat" "$LIFERAY_BASE/tomcat-$(cat tomcat_version)"
     fi
     cp -rv "$LIFERAY_BASE"/* "$LIFERAY_HOME"
+else
+    echo "$LIFERAY_BASE empty"
 fi
 
 if [[ "$(id -u)" = '0' ]] && [[ ( "$1" == *'catalina.sh' ) || ( "$3" == *'catalina.sh'* ) ]]; then
