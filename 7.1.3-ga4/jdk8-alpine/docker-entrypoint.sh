0f3dc63a701326247301e30de3ad4bf97e4808b5
Igor Baiborodine <igor-baiborodine@yandex.com>
2019-06-07 14:27:42 +0000

    Test TraviS CI integration

--- 7.1.3-ga4/jdk8-alpine/docker-entrypoint.sh
+++ 7.1.3-ga4/jdk8-alpine/docker-entrypoint.sh
@@ -1,23 +1,11 @@
-#!/bin/bash
-set -e
-
-printenv
-
-echo "
-cmd: $0
-args: $@
-Docker user ID:$(id -u)
-tomcat_version:$(cat tomcat_version)"
+#!/usr/bin/env bash
+set -eu
 
 if [[ "$(ls -A $LIFERAY_BASE)" ]]; then
-    tree --noreport "$LIFERAY_BASE"
-
     if [[ -d "$LIFERAY_BASE/tomcat" ]]; then
         mv "$LIFERAY_BASE/tomcat" "$LIFERAY_BASE/tomcat-$(cat tomcat_version)"
     fi
     cp -rv "$LIFERAY_BASE"/* "$LIFERAY_HOME"
-else
-    echo "$LIFERAY_BASE empty"
 fi
 
 if [[ "$(id -u)" = '0' ]] && [[ ( "$1" == *'catalina.sh' ) || ( "$3" == *'catalina.sh'* ) ]]; then
