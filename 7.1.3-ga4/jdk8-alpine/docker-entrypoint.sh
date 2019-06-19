44e8dd4d9a80e6b4c0011e385ee258d8df83098c
Igor Baiborodine <igor-baiborodine@yandex.com>
2019-06-19 14:20:06 +0000

    Update Dockerfile-alpine.template

--- 7.1.3-ga4/jdk8-alpine/docker-entrypoint.sh
+++ 7.1.3-ga4/jdk8-alpine/docker-entrypoint.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ "$(ls -A $LIFERAY_BASE)" ]]; then
-    if [[ -d "$LIFERAY_BASE/tomcat" ]]; then
-        mv "$LIFERAY_BASE/tomcat" "$LIFERAY_BASE/tomcat-$(cat tomcat_version)"
-    fi
-    cp -rv "$LIFERAY_BASE"/* "$LIFERAY_HOME"
+if [ "$(ls -A $LIFERAY_BASE)" ]; then
+  if [ -d "$LIFERAY_BASE/tomcat" ]; then
+    path="$(readlink -f $LIFERAY_HOME/tomcat)"
+    mv "$LIFERAY_BASE/tomcat" "$LIFERAY_BASE/$(basename $path)"
+  fi
+  cp -rv "$LIFERAY_BASE"/* "$LIFERAY_HOME"
 fi
 
-if [[ "$(id -u)" = '0' ]] && [[ ( "$1" == *'catalina.sh' ) || ( "$3" == *'catalina.sh'* ) ]]; then
-    chown -R liferay:liferay "$LIFERAY_HOME"
-    exec su-exec liferay "$@"
+if [ "$1" = 'catalina.sh' -a "$(id -u)" = '0' ]; then
+  chown -R liferay:liferay "$LIFERAY_HOME"
+  exec su-exec liferay "$@"
 fi
 
 exec "$@"
+
