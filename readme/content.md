0945cdd3c8cad64edec6cb1228fb0cca4c19abf6
Igor Baiborodine <igor.baiborodine@yandex.com>
2019-06-24 14:17:52 -0400

    Update readme files [skip travis]

--- readme/content.md
+++ readme/content.md
@@ -1,6 +1,6 @@
 # What is Liferay Portal CE?
 
-
+**Liferay Portal ** is an open source portal framework for building web applications, websites, and portals. It also offers a CMS and may serve as an enterprise integration platform.  
 
 ![logo](/readme/logo.png)
  
--- readme/generate-readme.sh
+++ readme/generate-readme.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-REPO_URL='https://github.com/igor-baiborodine/dockerhub-test'
+REPO_URL='https://github.com/igor-baiborodine/docker-liferay-portal-ce'
 
 replace_field() {
   local target_file="$1"
@@ -29,7 +29,7 @@ main() {
     local commit="${t#*:}"
     echo "tag: $tag, commit: $commit"
 
-    tags_content='-  [`'"${tag/\//-}"'` (*'"${tag}/Dockerfile"'*)]('"https://github.com/igor-baiborodine/${REPO}/blob/${commit}/${tag}/Dockerfile"$')\n'"$tags_content"
+    tags_content='-  [`'"${tag/\//-}"'` (*'"${tag}/Dockerfile"'*)]('"${REPO_URL}/blob/${commit}/${tag}/Dockerfile"$')\n'"$tags_content"
   done
   echo "tags_content: $tags_content"
 
@@ -38,6 +38,7 @@ main() {
   replace_field ./README.md 'CONTENT' "$(cat "readme/content.md")"
   replace_field ./README.md 'VARIANT' "$(cat "readme/variant.md")"
   replace_field ./README.md 'LICENSE' "$(cat "readme/license.md")"
+  replace_field ./README.md 'IMAGE' 'ibaiborodine/liferay-portal-ce'
 }
 
 main "$@"
