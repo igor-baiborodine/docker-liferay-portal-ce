109f46f68433017c5ab0e7b4c15fcfe0ac79a6ff
igor-baiborodine <igor.baiborodin@intact.net>
2019-07-03 12:20:00 -0400

    Update .travis.yml and push-remote.sh [skip travis]

--- push-remote.sh
+++ push-remote.sh
@@ -4,15 +4,15 @@ main() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
-  git clone "https://github.com/igor-baiborodine/$GITHUB_REPO_NAME.git" "~/$GITHUB_REPO_NAME"
-  cp README.md supported-tags "~/$GITHUB_REPO_NAME"
+  git clone "https://github.com/$TRAVIS_REPO_SLUG.git" "~/build/$TRAVIS_REPO_SLUG"
+  cp README.md supported-tags "~/build/$TRAVIS_REPO_SLUG"
 
-  cd "~/$GITHUB_REPO_NAME"
+  cd "~/build/$TRAVIS_REPO_SLUG"
   git add README.md supported-tags
 
   git status
   git commit -m "$TRAVIS_COMMIT_MESSAGE [skip travis]"
-  git push "https://${TRAVIS_GITHUB_TOKEN}@github.com/igor-baiborodine/${GITHUB_REPO_NAME}" master  > /dev/null 2>&1
+  git push "https://$TRAVIS_GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG" master  > /dev/null 2>&1
 }
 
 main "$@"
